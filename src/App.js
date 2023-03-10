5fa1002e65a162a4be82784fcfccc99a50057a25
Marat1242 <fullstack_web_developer@mail.ru>
2023-03-11 02:06:39 +0300

    chlen

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class App extends Component {
+  render(){
+  return (
+    <div>
+      <h1> Chat ebat</h1>
+    </div>
+  );
+  }
+}
+
+export default App;
